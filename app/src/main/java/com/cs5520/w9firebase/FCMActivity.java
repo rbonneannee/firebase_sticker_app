b95c605d781ebb4fc6249c3a03acada372f367f9
Rebecca Bonne-Annee <rebecca.m.bonneannee@gmail.com>
2020-07-03 09:56:15 -0400

    Add 'key=' to server key string

--- app/src/main/java/com/cs5520/w9firebase/FCMActivity.java
+++ app/src/main/java/com/cs5520/w9firebase/FCMActivity.java
@@ -1,5 +1,5 @@
 package com.cs5520.w9firebase;
 
 public class FCMActivity {
-    private static final String SERVER_KEY = "AAAAgmQuzxA:APA91bFW8VCokyXik3ngcV7mKS-UHUWrUqvE08pZOpy1AHszBih88Tvw8t0ciEc0BN3hGk_8810tB-yjx8zQVxhAjjxiYuLZPh91hsfgSarEN5Fn8ikLDfG2k6JZWGDwFg1rlRkwtXtV";
+    private static final String SERVER_KEY = "key=AAAAgmQuzxA:APA91bFW8VCokyXik3ngcV7mKS-UHUWrUqvE08pZOpy1AHszBih88Tvw8t0ciEc0BN3hGk_8810tB-yjx8zQVxhAjjxiYuLZPh91hsfgSarEN5Fn8ikLDfG2k6JZWGDwFg1rlRkwtXtV";
 }
