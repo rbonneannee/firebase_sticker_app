2b9b28d6cf676ef43752fa668647b632dd9cdccb
Rebecca Bonne-Annee <rebecca.m.bonneannee@gmail.com>
2020-07-07 08:00:46 -0400

    Store files before reinstalling Android Studio

--- app/src/main/java/com/cs5520/w9firebase/CloudMsgActivity.java
+++ app/src/main/java/com/cs5520/w9firebase/CloudMsgActivity.java
@@ -6,25 +6,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.cs5520.w9firebase.realtimedatabase.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CloudMsgActivity extends AppCompatActivity {
     private static final String TAG = CloudMsgActivity.class.getSimpleName();
+
+    private DatabaseReference database;
+    private Spinner spinner;
+    private List<User> userList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud_msg);
+        this.database = FirebaseDatabase.getInstance().getReference().child("users");
+        this.spinner = (Spinner) findViewById(R.id.spinner);
+        this.userList = new ArrayList<User>();
 
+
+ /*
         // Process to get application's unique Firebase instance token
         Button button_getToken = (Button) findViewById(R.id.button_getToken);
         button_getToken.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +67,7 @@ public class CloudMsgActivity extends AppCompatActivity {
                         });
             }
         });
-
+*/
 
 
     }
---
+++ app/src/main/java/com/cs5520/w9firebase/realtimedatabase/models/Message.java
@@ -0,0 +1,43 @@
+package com.cs5520.w9firebase.realtimedatabase.models;
+
+public class Message {
+
+    private User sender;
+    private User recipient;
+    private String title;
+    private String body;
+
+    public Message() {}
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
