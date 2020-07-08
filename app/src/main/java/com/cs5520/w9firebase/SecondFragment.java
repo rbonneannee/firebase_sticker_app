68f42d738666e612ec7521be4d07614885e8b970
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-07-08 12:20:41 -0400

    intent fix

--- app/src/main/java/com/cs5520/w9firebase/SecondFragment.java
+++ app/src/main/java/com/cs5520/w9firebase/SecondFragment.java
@@ -39,7 +39,7 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
 //                NavHostFragment.findNavController(SecondFragment.this)
 //                        .navigate(R.id.action_SecondFragment_to_inboxActivity);
-                Intent inbox = new Intent(getActivity(), CloudMsgActivity.class);
+                Intent inbox = new Intent(getActivity(), InboxActivity.class);
                 startActivity(inbox);
             }
         });
