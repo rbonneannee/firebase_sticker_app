c5d9c71a231ecace69533d6b2db9bca9d747e10d
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-07-08 12:39:35 -0400

    author in intents 2

--- app/src/main/java/com/cs5520/w9firebase/InboxActivity.java
+++ app/src/main/java/com/cs5520/w9firebase/InboxActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class InboxActivity extends AppCompatActivity {
 
+    private String currentUserToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.currentUserToken = getIntent().getStringExtra("userToken");
         setContentView(R.layout.activity_inbox);
     }
 }
