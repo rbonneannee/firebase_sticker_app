d79e8a6707e22b43dff0e9883fd315b77d29f459
Rebecca Bonne-Annee <rebecca.m.bonneannee@gmail.com>
2020-07-08 12:02:31 -0400

    Add Inbox Activity

---
+++ app/src/main/java/com/cs5520/w9firebase/InboxActivity.java
@@ -0,0 +1,14 @@
+package com.cs5520.w9firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox);
+    }
+}
